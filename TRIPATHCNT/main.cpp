795c5296fb5e63e6d89de0b9ea626f70a1b00433
gale34 <gales34@gmail.com>
2017-08-01 22:36:16 +0900

    20170801
    ...

--- TRIPATHCNT/main.cpp
+++ TRIPATHCNT/main.cpp
@@ -5,8 +5,10 @@
 using namespace std;
 
 int getMaxPath(const vector<vector<int> > triangle, int x, int y);
+unsigned int countMaxPath(int x, int y);
 
 vector<vector<int> > path;
+vector<vector<unsigned int> > countPath;
 
 int main()
 {
@@ -24,14 +26,30 @@ int main()
         for(int j = 0; j < n; j++)
         {
             vector<int> row;
+            vector<unsigned int> row2;
             row.resize(j+1);
+            row2.resize(j+1);
             triangle.push_back(row);
+            countPath.push_back(row2);
             for(int k = 0; k < j+1; k++)
+            {
                 cin >> triangle[j][k];
+                countPath[j][k] = 0;
+            }
         }
         path = triangle;
+        getMaxPath(triangle, 0 ,0);
 
-        cout << getMaxPath(triangle, 0 ,0) << endl;
+        cout << countMaxPath(0,0) << endl;
+
+        /*for(int j = 0; j < n; j++)
+        {
+            for(int k = 0; k < j+1; k++)
+            {
+                cout << path[j][k] << " ";
+            }
+            cout << endl;
+        }*/
 
         for(int j = 0; j < (int)path.size(); j++)
             path[j].clear();
@@ -40,6 +58,10 @@ int main()
         for(int j = 0; j < (int)triangle.size(); j++)
             triangle[j].clear();
         triangle.clear();
+
+        for(int j = 0; j < (int)countPath.size(); j++)
+            countPath[j].clear();
+        countPath.clear();
     }
     return 0;
 }
@@ -67,6 +89,26 @@ int getMaxPath(const vector<vector<int> > triangle,int x, int y)
         answer += answerB;
 
     path[x][y] = answer;
+
+    return answer;
+}
+
+unsigned int countMaxPath(int x, int y)
+{
+    unsigned int& answer = countPath[x][y];
+
+    if(answer != 0)
+        return answer;
+
+    if(x == (int)countPath.size() - 1)
+        return answer = 1;
+
+    if(path[x+1][y] > path[x+1][y+1])
+        answer = max(answer, countMaxPath(x+1,y));
+    else if(path[x+1][y] < path[x+1][y+1])
+        answer = max(answer, countMaxPath(x+1,y+1));
+    else
+        answer = max(answer, countMaxPath(x+1,y) + countMaxPath(x+1,y+1));
 
     return answer;
 }
