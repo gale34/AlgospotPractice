b858897d5690f7f91cbe75ae97bae898b2a62dd7
gale34 <gales34@gmail.com>
2017-08-30 22:02:00 +0900

    170830
    ...

---
+++ OCR/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    freopen("input.txt","r",stdin);
+
+
+    cout << "Hello world!" << endl;
+    return 0;
+}
