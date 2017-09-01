341bcc81ed5fae37ae2a74c884c76b2dd6e39c36
gale34 <gales34@gmail.com>
2017-09-01 21:42:22 +0900

    170901
    ...

--- OCR/main.cpp
+++ OCR/main.cpp
@@ -9,7 +9,72 @@ int main()
 {
     freopen("input.txt","r",stdin);
 
+    int words, seqs;
+    string* texts;
+    double* firstChance;
+    double** nextChance;
+    double** missChance;
 
-    cout << "Hello world!" << endl;
+    cin >> words >> seqs;
+
+    texts = new string[words];
+
+    for(int i = 0; i < words; i++)
+        cin >> texts[i];
+
+    firstChance = new double[words];
+    nextChance = new double*[words];
+    missChance = new double*[words];
+
+    for(int i = 0; i < words; i++)
+    {
+        nextChance[i] = new double[words];
+        missChance[i] = new double[words];
+        memset(nextChance[i], 0, sizeof(double) * words);
+        memset(missChance[i], 0, sizeof(double) * words);
+    }
+
+    for(int i = 0; i < words; i++)
+        cin >> firstChance[i];
+
+
+    for(int i = 0; i < words; i++)
+    {
+
+        for(int j = 0; j < words; j++)
+            cin >> nextChance[i][j];
+    }
+    for(int i = 0; i < words; i++)
+    {
+        for(int j = 0; j < words; j++)
+            cin >> missChance[i][j];
+    }
+
+    for(int i = 0; i < seqs; i++)
+    {
+        int n;
+        string* example;
+        cin >> n;
+
+        example = new string[n];
+
+        for(int j = 0; j < n; j++)
+            cin >> example[j];
+
+        cout << "Hello world!" << endl;
+
+        delete example;
+    }
+
+    delete firstChance;
+    for(int i = 0; i < words; i++)
+    {
+        delete [] nextChance[i];
+        delete [] missChance[i];
+    }
+
+    delete [] nextChance;
+    delete [] missChance;
+
     return 0;
 }
