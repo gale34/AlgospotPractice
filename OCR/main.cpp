a9cb0e9006f0b7a9f3f93b74b306c41bc81feab1
gale34 <gales34@gmail.com>
2017-09-02 21:00:42 +0900

    20170902
    Too dificult....TT

--- OCR/main.cpp
+++ OCR/main.cpp
@@ -5,13 +5,14 @@
 
 using namespace std;
 
+double MaxProbSeq(double** memory, int oriNwords, string* original, double** nextChance, double** missChance, string* example, int examNum, int n, int w);
+
 int main()
 {
     freopen("input.txt","r",stdin);
 
     int words, seqs;
     string* texts;
-    double* firstChance;
     double** nextChance;
     double** missChance;
 
@@ -22,25 +23,27 @@ int main()
     for(int i = 0; i < words; i++)
         cin >> texts[i];
 
-    firstChance = new double[words];
-    nextChance = new double*[words];
+    nextChance = new double*[words+1];
     missChance = new double*[words];
+
+    for(int i = 0; i < words + 1; i++)
+    {
+        nextChance[i] = new double[words];
+        memset(nextChance[i], 0, sizeof(double) * words);
+    }
 
     for(int i = 0; i < words; i++)
     {
-        nextChance[i] = new double[words];
         missChance[i] = new double[words];
-        memset(nextChance[i], 0, sizeof(double) * words);
         memset(missChance[i], 0, sizeof(double) * words);
     }
 
     for(int i = 0; i < words; i++)
-        cin >> firstChance[i];
+        cin >> nextChance[0][i];
 
 
-    for(int i = 0; i < words; i++)
+    for(int i = 1; i < words + 1; i++)
     {
-
         for(int j = 0; j < words; j++)
             cin >> nextChance[i][j];
     }
@@ -56,6 +59,17 @@ int main()
         string* example;
         cin >> n;
 
+        double** memory;
+        memory = new double[101];
+        for(int j = 0; j < 101; j++)
+            memory[j] = new double[words+1];
+
+        for(int j = 0; j < 101; j++)
+        {
+            for(int k = 0; k < words + 1; k++)
+                memory[j][k] = -1;
+        }
+
         example = new string[n];
 
         for(int j = 0; j < n; j++)
@@ -64,17 +78,58 @@ int main()
         cout << "Hello world!" << endl;
 
         delete example;
+
+        for(int j = 0; j < 101; j++)
+            delete [] memory[j];
+        delete [] memory;
     }
 
     delete firstChance;
+
+
     for(int i = 0; i < words; i++)
     {
         delete [] nextChance[i];
         delete [] missChance[i];
     }
 
+
     delete [] nextChance;
     delete [] missChance;
 
     return 0;
+}
+
+double MaxProbSeq(double** memory, int oriNwords, string* original, double** nextChance, double** missChance, string* example,int examNum, int n, int w)
+{
+    if(n == 0)
+        return nextChance[n][w];
+
+    int target = -1;
+    double& answer = memory[n][w];
+    double temp = 0;
+    if(answer != -1)
+        return answer;
+
+    answer = 0;
+
+    for(int i = 0; i < oriNwords; i++)
+    {
+        answer += nextChance[i][w] * MaxProbSeq(memory,oriNwords,original,nextChance,missChance,example,examNum, n-1 , i);
+    }
+
+    for(int i = 0; i < oriNwords; i++)
+    {
+        if(original[i].compare(example[n-1]) == 0)
+        {
+            target = i;
+            break;
+        }
+    }
+
+        missChance[w][target]
+
+
+    return answer;
+
 }
