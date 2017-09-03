666a0d240113279b1a55948296e7af852bd7a0f0
gale34 <gales34@gmail.com>
2017-09-03 22:15:29 +0900

    20170903
    To be Continued...

---
+++ MORSE/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+int countMorse = 0;
+
+int main()
+{
+    int c;
+    freopen("input.txt","r",stdin);
+
+    cin >> c;
+
+    for(int i = 0; i < c; i++)
+    {
+        int n,m,k;
+
+        cin >> n >> m >> k;
+    }
+    cout << "Hello world!" << endl;
+    return 0;
+}
+
+
